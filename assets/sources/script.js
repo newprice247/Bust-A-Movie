d8abd16d8d48372211b39f6f15ed29d54e5458eb
newprice247 <newprice247@gmail.com>
2023-08-08 19:44:53 -0400

    add movie page to app

--- assets/sources/script.js
+++ assets/sources/script.js
@@ -28,22 +28,42 @@ $('#searchButton').on('click', function() {
 
 //Search function
 var movieSearch = (title) => {
-    console.log('Search button clicked')
-    console.log(title)
-    var imdbID
-    var movieDatabaseApi = `http://www.omdbapi.com/?t==${title}&apikey=611f00c7`
-    var watchmodeStreamingApi =  `https://api.watchmode.com/v1/title/${imdbID}/details/?apiKey=6N5wEhqG1MjX7EYLU4zvfMui5TyhL4Io8eUxuhM5&append_to_response=sources`
+    console.log('Search button clicked');
+    console.log(title);
+    // var imdbID
+    var movieDatabaseApi = `http://www.omdbapi.com/?t==${title}&apikey=611f00c7`;
+    
+    
+    // var imdbID = ''
+
     fetch(movieDatabaseApi)
         .then(function (response) {
             return response.json()
         })
         .then(function (data) {
-            imdbID = data.imdbID
-            console.log(data)
-            // return imdbID
+            imdbID = data.imdbID;
+            console.log(data);
+            console.log(data.Title);
+            console.log(data.Plot);
+            $('#searchForMovie').html(`
+            <p class="is-size-3">It worked!</p>
+            <div class="box">
+                <p>Title: ${data.Title}</p>
+                <p>Year:  ${data.Year}</p>
+                <img src="${data.Poster}" alt="Movie Poster">
+                <p>Ratings:</p>
+                <p>IMDb: ${data.Ratings[0].Value}</p>
+                <p>Rotten Tomatoes: ${data.Ratings[1].Value}.</p>
+                <p>Metacritic: ${data.Ratings[2].Value}</p>
+                
+            </div>
+            `)
         }) 
         // console.log(imdbID)
     
+    
+    // var watchmodeStreamingApi =  `https://api.watchmode.com/v1/title/${imdbID}/details/?apiKey=6N5wEhqG1MjX7EYLU4zvfMui5TyhL4Io8eUxuhM5&append_to_response=sources`
+
     // fetch(watchmodeStreamingApi)
     //     .then(function (response) {
     //         return response.json()
@@ -54,3 +74,79 @@ var movieSearch = (title) => {
 
 }       
 
+// {Title: 'Up', Year: '2009', Rated: 'PG', Released: '29 May 2009', Runtime: '96 min', …}
+// Actors
+// : 
+// "Edward Asner, Jordan Nagai, John Ratzenberger"
+// Awards
+// : 
+// "Won 2 Oscars. 80 wins & 87 nominations total"
+// BoxOffice
+// : 
+// "$293,004,164"
+// Country
+// : 
+// "United States, Japan"
+// DVD
+// : 
+// "01 Jan 2014"
+// Director
+// : 
+// "Pete Docter, Bob Peterson"
+// Genre
+// : 
+// "Animation, Adventure, Comedy"
+// Language
+// : 
+// "English"
+// Metascore
+// : 
+// "88"
+// Plot
+// : 
+// "78-year-old Carl Fredricksen travels to Paradise Falls in his house equipped with balloons, inadvertently taking a young stowaway."
+// Poster
+// : 
+// "https://m.media-amazon.com/images/M/MV5BYjBkM2RjMzItM2M3Ni00N2NjLWE3NzMtMGY4MzE4MDAzMTRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg"
+// Production
+// : 
+// "N/A"
+// Rated
+// : 
+// "PG"
+// Ratings
+// : 
+// (3) [{…}, {…}, {…}]
+// Released
+// : 
+// "29 May 2009"
+// Response
+// : 
+// "True"
+// Runtime
+// : 
+// "96 min"
+// Title
+// : 
+// "Up"
+// Type
+// : 
+// "movie"
+// Website
+// : 
+// "N/A"
+// Writer
+// : 
+// "Pete Docter, Bob Peterson, Tom McCarthy"
+// Year
+// : 
+// "2009"
+// imdbID
+// : 
+// "tt1049413"
+// imdbRating
+// : 
+// "8.3"
+// imdbVotes
+// : 
+// "1,080,506"
