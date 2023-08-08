61fb8145dbf6ed3046e65eb4b0e126f7250a2669
newprice247 <newprice247@gmail.com>
2023-08-08 17:13:46 -0400

    omdb api integrated

--- assets/sources/script.js
+++ assets/sources/script.js
@@ -1,3 +1,10 @@
+//API keys
+// var movieDatabaseApi = `http://www.omdbapi.com/?t==${title}&apikey=611f00c7`
+// var watchmodeStreamingApi =  `https://api.watchmode.com/v1/title/345534/details/?apiKey=6N5wEhqG1MjX7EYLU4zvfMui5TyhL4Io8eUxuhM5&append_to_response=sources`
+
+
+
+
 
 //Allows enter key to be pressed to start search
 $("#searchBar").keypress(function (event) {
@@ -9,7 +16,10 @@ $("#searchBar").keypress(function (event) {
 
 //Event Listener for the search button
 $('#searchButton').on('click', function() {
-    movieSearch()
+    //pulls the text entered into the searchbar and saves it as the variable 'title'
+    var title = $('#searchBar').val()
+    //calls the movieSearch function and searches for the title of the movie
+    movieSearch(title)
     //If statement for the search field being empty when the button is clicked
     if ($('#searchBar').val() === "") {
         console.log('Please enter a movie title')
@@ -17,9 +27,30 @@ $('#searchButton').on('click', function() {
 })
 
 //Search function
-var movieSearch = () => {
+var movieSearch = (title) => {
     console.log('Search button clicked')
-    var title = $('#searchBar').val()
     console.log(title)
-}
+    var imdbID
+    var movieDatabaseApi = `http://www.omdbapi.com/?t==${title}&apikey=611f00c7`
+    var watchmodeStreamingApi =  `https://api.watchmode.com/v1/title/${imdbID}/details/?apiKey=6N5wEhqG1MjX7EYLU4zvfMui5TyhL4Io8eUxuhM5&append_to_response=sources`
+    fetch(movieDatabaseApi)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            imdbID = data.imdbID
+            console.log(imdbID)
+            // return imdbID
+        }) 
+        // console.log(imdbID)
+    
+    // fetch(watchmodeStreamingApi)
+    //     .then(function (response) {
+    //         return response.json()
+    //     })
+    //     .then(function (data) {
+    //         console.log(data)
+    //     })
+
+}       
 
