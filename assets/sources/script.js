c4333f2993798a2307210defbd6acf4cef9dea7e
newprice247 <newprice247@gmail.com>
2023-08-10 20:02:08 -0400

    add search results

--- assets/sources/script.js
+++ assets/sources/script.js
@@ -30,79 +30,89 @@ $('#searchButton').on('click', function() {
 var movieSearch = (title) => {
     console.log('Search button clicked');
     console.log(title);
-    var movieDatabaseApi = `http://www.omdbapi.com/?t==${title}&page=1&apikey=611f00c7`;
+    var movieDatabaseSearch = `http://www.omdbapi.com/?s=${title}&page=1&apikey=611f00c7`;
+
+    // var movieDatabaseApi = `http://www.omdbapi.com/?t==${title}&page=1&apikey=611f00c7`;
     
-    fetch(movieDatabaseApi)
+    fetch(movieDatabaseSearch)
         .then(function (response) {
             return response.json()
         })
-        .then(function (data) {
-            // var searchResults = (data.Search)
-            console.log(data)
-            // for (var i=0; i<5; i++){
-            //     console.log(searchResults[i].Title)
-            // }
-            var name = data.Title;
-            var nameArr = name.split(' ')
-            let nameString = nameArr.join('_')
-            console.log(nameString)
-            console.log(name)
-            console.log(nameArr)
-            console.log(data);
-            console.log(data.Title);
-            console.log(data.Plot);
+        .then(function (searchData) {
+            console.log(searchData)
+            var searchResults = (searchData.Search)
+            for (var i=0; i<5; i++){
+                var poster = searchResults[i].Poster
+                console.log(searchResults)
+                $('#searchResults').append(`
+                    <div class="column has-background-dark">
+                        <p>${searchResults[i].Title}</p>
+                            <a target="_blank" href="#">
+                            <img src="${poster}">
+                        </a>
+                    </div>
+                `)
+                console.log(searchResults[i].Title)
+            }
+        })
+        
+        // .then(function (data) {
             
-            $('#searchForMovie').html(`
-            <p class="is-size-3">It worked!</p>
-            <div class="box">
-                <p>Title: ${data.Title}</p>
-                <p>Year:  ${data.Year}</p>
-                <p>Rated: ${data.Rated}</p>
-                <img src="${data.Poster}" alt="Movie Poster">
-                <p>Genre: ${data.Genre}</p>
-                <div id="ratingsBox">
-                    <p>Ratings:</p>
-                    <p>Rotten Tomatoes: ${data.Ratings[1].Value}.</p>
-                    <a target="_blank" href="https://www.rottentomatoes.com/m/${nameString}">
-                        <img src="./assets/images/Rotten_Tomatoes_logo.svg.png" alt="Movie Poster">
-                    </a>
-                   <p>IMDb: ${data.Ratings[0].Value}</p>
+        //     console.log(data)
+            
+        //     var name = data.Title;
+        //     var nameArr = name.split(' ')
+        //     let nameString = nameArr.join('_')
+        //     console.log(nameString)
+        //     console.log(name)
+        //     console.log(nameArr)
+        //     console.log(data);
+        //     console.log(data.Title);
+        //     console.log(data.Plot);
+            
+        //     $('#searchForMovie').html(`
+        //     <p class="is-size-3">It worked!</p>
+        //     <div class="box">
+        //         <p>Title: ${data.Title}</p>
+        //         <p>Year:  ${data.Year}</p>
+        //         <p>Rated: ${data.Rated}</p>
+        //         <img src="${data.Poster}" alt="Movie Poster">
+        //         <p>Genre: ${data.Genre}</p>
+        //         <div id="ratingsBox">
+        //             <p>Ratings:</p>
+        //             <p>Rotten Tomatoes: ${data.Ratings[1].Value}.</p>
+        //             <a target="_blank" href="https://www.rottentomatoes.com/m/${nameString}">
+        //                 <img src="./assets/images/Rotten_Tomatoes_logo.svg.png" alt="Movie Poster">
+        //             </a>
+        //            <p>IMDb: ${data.Ratings[0].Value}</p>
                    
-                    <a target="_blank" href="https://www.imdb.com/title/${data.imdbID}/">
-                        <img src="./assets/images/IMDB_Logo.png" alt="Movie Poster">
-                    </a>
-                </div>
-            </div>
-            `)
-            return data;
-        })
-        .then(function (data) {
-            var name = data.Title;
-            var nameArr = name.split(' ')
-            let nameString = nameArr.join('-').toLowerCase()
-            console.log(nameString.toLowerCase())
-            $('#ratingsBox').append(`
-                    <p>Metacritic: ${data.Ratings[2].Value}</p>
-                    <a target="_blank" href="https://www.metacritic.com/movie/${nameString}">
-                            <img src="./assets/images/Metacritic_logo2.png" alt="Movie Poster">
-                    </a>`)
-            return fetch(`https://api.watchmode.com/v1/title/${data.imdbID}/details/?apiKey=6N5wEhqG1MjX7EYLU4zvfMui5TyhL4Io8eUxuhM5&append_to_response=sources`)
-        })
-        .then(response => {
-            return response.json();
-        })
-        .then(function (data2) {
-            console.log(data2)
-        })
-        .catch(error => {
-            console.error(error);
-          });
+        //             <a target="_blank" href="https://www.imdb.com/title/${data.imdbID}/">
+        //                 <img src="./assets/images/IMDB_Logo.png" alt="Movie Poster">
+        //             </a>
+        //         </div>
+        //     </div>
+        //     `)
+        //     return data;
+        // })
+        // .then(function (data) {
+        //     var name = data.Title;
+        //     var nameArr = name.split(' ')
+        //     let nameString = nameArr.join('-').toLowerCase()
+        //     console.log(nameString.toLowerCase())
+        //     $('#ratingsBox').append(`
+        //             <p>Metacritic: ${data.Ratings[2].Value}</p>
+        //             <a target="_blank" href="https://www.metacritic.com/movie/${nameString}">
+        //                     <img src="./assets/images/Metacritic_logo2.png" alt="Movie Poster">
+        //             </a>`)
+        //     return fetch(`https://api.watchmode.com/v1/title/${data.imdbID}/details/?apiKey=6N5wEhqG1MjX7EYLU4zvfMui5TyhL4Io8eUxuhM5&append_to_response=sources`)
+        // })
+        // .then(response => {
+        //     return response.json();
+        // })
+        // .then(function (data2) {
+        //     console.log(data2)
+        // })
+        // // .catch(error => {
+        // //     console.error(error);
+        // //   });
 }       
-
-console.log('different')
-
-console.log('git pull')
-
-console.log('git fetch --all')
-
-console.log('nathan')
