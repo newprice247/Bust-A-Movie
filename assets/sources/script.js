4fd26678c73dfd87d4a176ba0fd6e98a3a8d72e4
newprice247 <newprice247@gmail.com>
2023-08-17 11:18:49 -0400

    added comments

--- assets/sources/script.js
+++ assets/sources/script.js
@@ -1,5 +1,6 @@
+// Object used to store the local storage data while the site is active
 var viewHistory = { search: [], id: [], poster: []}
-
+// On loading the page will pull data from local storage and store it in the viewHistory object, then create the 'Your Movies' page for the user
 function onLoad() {
     if (localStorage.getItem('history')) {
         viewHistory = JSON.parse(localStorage.getItem('history'));
@@ -10,7 +11,7 @@ function onLoad() {
                     ${viewHistory.search[i]}
                 </p>
                 <a>
-                <img id="${viewHistory.id[i]}" class="recentSearchItem" src="https://image.tmdb.org/t/p/w300/${viewHistory.poster[i]}" alt="${viewHistory.search[i]}}">
+                    <img id="${viewHistory.id[i]}" class="recentSearchItem" src="https://image.tmdb.org/t/p/w300/${viewHistory.poster[i]}" alt="${viewHistory.search[i]}}">
                 </a>
             </div>
             `)
@@ -34,7 +35,7 @@ $(".searchBar").keypress(function (event) {
         $(".searchButton").click();
     }
 });
-//Event Listener for the search button
+//Event Listener for the search button in navbar
 $('#navSearchButton').on('click', function () {
     var title = $('#navSearchBar').val()
     if (title === "") {
@@ -51,6 +52,7 @@ $('#navSearchButton').on('click', function () {
     $('#navSearch').show()
 
 })
+//Event Listener for the search button in hero banner
 $('#heroSearchButton').on('click', function () {
     var title = $('#heroSearchBar').val()
     if (title === "") {
@@ -67,7 +69,7 @@ $('#heroSearchButton').on('click', function () {
     $('#navSearch').show()
 
 })
-//Event listener for the navbar buttons
+//Event listeners for the navbar buttons
 $('#homePage').on('click', function () {
     $('#aboutPage').hide()
     $('#favMovies').show()
@@ -77,7 +79,6 @@ $('#homePage').on('click', function () {
     $('.hero').show()
     $('#navSearch').hide()
 })
-
 $('#about').on('click', function () {
     $('#aboutPage').show()
     $('#favMovies').hide()
@@ -86,7 +87,6 @@ $('#about').on('click', function () {
     $('#yourMovies').hide()
     $('.hero').hide()
 })
-
 $('#yourMoviesNav').on('click', function () {
     $('#aboutPage').hide()
     $('#yourMovies').show()
@@ -96,13 +96,13 @@ $('#yourMoviesNav').on('click', function () {
     $('#resultsPage').hide()
     
 })
-
+// Event listener for the movies the user has saved to the 'Your Movies' page
 $('.recentSearchItem').on('click', function (event) {
     var id = $(this).attr('id')
     showResults(id)
     event.stopPropagation()
 })
-
+// Event Listener for the movies located in the 'Popular Searches' section of the homepage
 $('.favMovieBox').on('click', function () {
     $('#displayResults').html("")
     $('#aboutPage').hide()
@@ -112,7 +112,7 @@ $('.favMovieBox').on('click', function () {
     var id = $(this).attr('id')
     showResults(id)
 })
-
+// Event Listener for the clear history button in the 'Your Movies' page
 $('#clearHistory').on('click', function() {
     localStorage.clear()
     $('#recentSearches').html('')
@@ -122,7 +122,7 @@ $('#clearHistory').on('click', function() {
         `)
 })
 
-//Search function
+//Searches for movie based on the value of the text entered into either searchbar
 var movieSearch = (title) => {
     var watchmodeSearch = `https://api.watchmode.com/v1/autocomplete-search/?apiKey=h9bYrpSa7Rlr9oEUoXUqIQj14GFbRQy3LF7JvrEI&search_value=${title}&search_type=2`;
     fetch(watchmodeSearch)
@@ -136,6 +136,7 @@ var movieSearch = (title) => {
             $('#searchSectionTitle').text(`
                 Showing Results for "${title}"
             `)
+            // for loop that loops through all the movies that the api search call returns, determines if they are popular enough to have both a poster image and TMDb id, then appends the filtered results to the page
             var searchResults = (searchData.results)
             for (var i = 0; i < searchResults.length; i++) {
                 var poster = searchResults[i].image_url
@@ -157,7 +158,7 @@ var movieSearch = (title) => {
                     </div>
                 `)
             }
-
+            // Event Listener for the search results, will call the showResults function
             $('.resultButton').on('click', function () {
                 var id = $(this).parents('.box').attr('id')
                 showResults(id)
@@ -167,7 +168,7 @@ var movieSearch = (title) => {
             console.log(error)
         })
 }
-
+// Displays the details of the movie the user selected from the search results page
 var showResults = (id) => {
     tmdbSearch = `https://api.themoviedb.org/3/movie/${id}?api_key=b3783de294fab53f3b5f107706f3d99e`;
     fetch(tmdbSearch)
@@ -206,6 +207,7 @@ var showResults = (id) => {
                 <p class="mb-3">${genres}</p>
             </div>
             `)
+            // Checks to see if the name of the resulting movie is stored in the viewHistory Object, stores the name, id, and poster path of the movie to the viewHistory object if it is not already, then appends the newly stored movie to the 'Your Movies' page
             if (viewHistory.search.includes(name) === false) {
                 viewHistory.search.push(name);
                 viewHistory.poster.push(poster);
@@ -221,26 +223,34 @@ var showResults = (id) => {
                         </a>
                     </div>
                 `)
+                // Changes the text of the clear history button if a new movie is added to the 'Your Movies' page
                 $('#clearHistory').html(`
                     <p class="is-size-4 has-text-danger">Clear History</p>
                 `)
+                // Creates an event listener for any new movies appended to the 'Your Movies' section
                 $('.recentSearchItem').on('click', function (event) {
                     var id = $(this).attr('id')
                     showResults(id)
                     event.stopPropagation()
                 })
             }
+            // Uses the imdb id that is available with the tmdb api to begin another api call to omdb, in a process known as chaining api's
             return fetch(`https://www.omdbapi.com/?i=${imdbId}&page=1&apikey=611f00c7`)
         })
         .then(response => {
             return response.json();
         })
+        // Uses OMDb data to display movie critic website ratings
         .then(data2 => {
+            // Restuctures the title of the movie so that it can be used inside of the search links to the review websites below
             var name = data2.Title
+            // If there is a semi-colon, it won't be included in the title string, as the websites don't recognise it the the url
             var newName = name.replace(':', '')
+            // Splits the .title string into separate words, then rejoins them with either underscores(for rotten tomatoes), or hyphens(for metacritic) separating the words
             var nameArr = newName.split(' ')
             var nameStringRotten = nameArr.join('_').toLowerCase()
             var nameStringMeta = nameArr.join('-').toLowerCase()
+            // Pulls even more data about the movie that wasn't available in the tmdb api
             $('#dataBox').append(`
                 <p class="is-size-4 has-text-warning">Director</p>
                 <p class="mb-3">${data2.Director}</p>
@@ -251,6 +261,7 @@ var showResults = (id) => {
                 <p class="is-size-4 has-text-warning">Box Office</p>
                 <p class="mb-3">${data2.BoxOffice}</p>
                 `)
+            // Appends the ratings scores and links to the movie's pages on those specific sites to the page
             $('#displayRatings').html('')
             $('#displayRatings').append(`
             <div id="ratingsBox" class="is-size-4">
@@ -275,12 +286,13 @@ var showResults = (id) => {
                 </div>
             </div>
         `)
-
+            // Chains the watchmode api with the imdb ID as the search parameter
             return fetch(`https://api.watchmode.com/v1/title/${data2.imdbID}/details/?apiKey=h9bYrpSa7Rlr9oEUoXUqIQj14GFbRQy3LF7JvrEI&append_to_response=sources`)
         })
         .then(response => {
             return response.json();
         })
+        // Displays the available streaming sites for the movie as links to those websites
         .then(data3 => {
 
             $('#streamingBox').show()
@@ -289,6 +301,7 @@ var showResults = (id) => {
                 <p class="is-size-3 mb-3 has-text-warning">Available Streaming Services:</p>
             `)
             var streamingArr = { title: [], url: [] }
+            // Checks to make sure there are no repetitions of streaming sources(i.e. three youtube data points, one for renting SD, one for renting 4K, and one for purchasing) and only chooses the first available source on that particular website
             for (i = 0; i < data3.sources.length; i++) {
                 if (streamingArr.title.includes(data3.sources[i].name) === false) {
                     streamingArr.title.push(data3.sources[i].name);
@@ -307,7 +320,7 @@ var showResults = (id) => {
 }
 
 
-//API keys
+//API keys (So we have easy access to them or if they get lost or deleted)
 // var movieDatabaseApi = `http://www.omdbapi.com/?t==${title}&apikey=611f00c7`
 // var watchmodeStreamingApi =  `https://api.watchmode.com/v1/title/345534/details/?apiKey=6N5wEhqG1MjX7EYLU4zvfMui5TyhL4Io8eUxuhM5&append_to_response=sources`
 // backup watchmode key h9bYrpSa7Rlr9oEUoXUqIQj14GFbRQy3LF7JvrEI
